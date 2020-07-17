6df21a89df8c6e8a168a3aeffa72d3425106dfa6
Mirailaopo <1597784057@qq.com>
2020-07-17 22:50:45 +0800

    Site updated: 2020-07-17 22:50:42

--- gk_assets/js/script.js
+++ gk_assets/js/script.js
@@ -6,7 +6,7 @@ var clock;
 				var currentDate = new Date();
 
 				// Set some date in the future.
-				var futureDate  = new Date(2021, 06, 07);
+				var futureDate  = new Date(2021, 05, 07);
 
 				// Calculate the difference in seconds between the future and current date
 				var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
